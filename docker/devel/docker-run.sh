d83ff3feef0dbce116a7595fbe2165b08f7499f7
Alexander Entinger <consulting@lxrobotics.com>
2023-11-14 15:53:15 +0100

    Simplifying startup scripts.

--- docker/devel/docker-run.sh
+++ docker/devel/docker-run.sh
@@ -9,32 +9,21 @@ fi
 
 CAN=can0
 CAN_BITRATE=250000
-
 GPIO_CAN0_STBY=160
-GPIO_CAN1_STBY=161
-
-if [ "$CAN" = "can0" ]; then
-  GPIO_CAN_STBY=$GPIO_CAN0_STBY
-elif [ "$CAN" = "can1" ]; then
-  GPIO_CAN_STBY=$GPIO_CAN1_STBY
-else
-  echo "Error, invalid can interface specified"
-  exit 1
-fi
 
 echo "Configuring $CAN for a bitrate of $CAN_BITRATE bits/s"
 
 function finish
 {
   ip link set $CAN down
-  echo 1 > /sys/class/gpio/gpio$GPIO_CAN_STBY/value
-  echo $GPIO_CAN_STBY > /sys/class/gpio/unexport
+  echo 1 > /sys/class/gpio/gpio$GPIO_CAN0_STBY/value
+  echo $GPIO_CAN0_STBY > /sys/class/gpio/unexport
 }
 trap finish EXIT
 
-echo $GPIO_CAN_STBY > /sys/class/gpio/export
-echo out > /sys/class/gpio/gpio$GPIO_CAN_STBY/direction
-echo 0 > /sys/class/gpio/gpio$GPIO_CAN_STBY/value
+echo $GPIO_CAN0_STBY > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio$GPIO_CAN0_STBY/direction
+echo 0 > /sys/class/gpio/gpio$GPIO_CAN0_STBY/value
 
 ip link set $CAN type can bitrate $CAN_BITRATE
 ip link set $CAN up
